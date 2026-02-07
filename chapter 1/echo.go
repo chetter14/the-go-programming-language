cbc1e69ebb424c852c38c30d655fbf7831fcadfd
Artem Leshchukov <artemleshchukov02@gmail.com>
2026-02-08 02:39:47 +0300

    Complete up to 1.2

---
+++ chapter 1/echo.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+// func main() {
+// // Echo the cmd arguments
+// var s, sep string
+// for i := 1; i < len(os.Args); i++ {
+// s += sep + os.Args[i]
+// sep = " "
+
+// for i < 0 {
+// fmt.Println("It's never going to be printed")
+// }
+// }
+// fmt.Println(s)
+
+// }
+
+// func main() {
+// // Short variable declaration
+// s, sep := "", ""
+// // Unused value in '_'
+// for _, arg := range os.Args[1:] {
+// s += sep + arg
+// sep = " "
+// }
+// fmt.Println(s)
+// }
+
+func main() {
+	// fmt.Println(strings.Join(os.Args[1:], " "))
+	fmt.Println(os.Args[1:])
+}
