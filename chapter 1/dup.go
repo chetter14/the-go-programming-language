564daef5c2267470f3256854fb29c3d9ee94f946
Artem Leshchukov <artemleshchukov02@gmail.com>
2026-02-09 20:28:52 +0300

    Complete finding duplicated line

--- chapter 1/dup.go
+++ chapter 1/dup.go
@@ -1,25 +1,67 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
+	"io/ioutil"
 	"os"
+	"strings"
 )
+
+// func main() {
+// counts := make(map[string]int)
+// files := os.Args[1:]
+// if len(files) == 0 {
+// countLines(os.Stdin, counts)
+// } else {
+// for _, arg := range files {
+// f, err := os.Open(arg)
+// if err != nil {
+// fmt.Fprintf(os.Stderr, "dup: %v\n", err)
+// continue
+// }
+// countLines(f, counts)
+// f.Close()
+// }
+// }
+
+// for line, n := range counts {
+// if n > 1 {
+// fmt.Printf("%d\t%s\n", n, line)
+// }
+// }
+// }
+
+// func countLines(f *os.File, counts map[string]int) {
+// input := bufio.NewScanner(f)
+// for input.Scan() {
+// counts[input.Text()]++
+// }
+// }
 
 func main() {
 	counts := make(map[string]int)
-	files := os.Args[1:]
-	if len(files) == 0 {
-		countLines(os.Stdin, counts)
-	} else {
-		for _, arg := range files {
-			f, err := os.Open(arg)
-			if err != nil {
-				fmt.Fprintf(os.Stderr, "dup: %v\n", err)
-				continue
+	for _, filename := range os.Args[1:] {
+		data, err := ioutil.ReadFile(filename)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "dup3: %v\n", err)
+			continue
+		}
+		for _, line := range strings.Split(string(data), "\n") {
+			counts[line]++
+		}
+	}
+
+	for _, filename := range os.Args[1:] {
+		data, err := ioutil.ReadFile(filename)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "dup3: %v\n", err)
+			continue
+		}
+		for _, line := range strings.Split(string(data), "\n") {
+			if counts[line] > 1 {
+				fmt.Printf("dup3: %s contains duplicated line\n", filename)
+				break
 			}
-			countLines(f, counts)
-			f.Close()
 		}
 	}
 
@@ -28,11 +70,5 @@ func main() {
 			fmt.Printf("%d\t%s\n", n, line)
 		}
 	}
-}
 
-func countLines(f *os.File, counts map[string]int) {
-	input := bufio.NewScanner(f)
-	for input.Scan() {
-		counts[input.Text()]++
-	}
 }
