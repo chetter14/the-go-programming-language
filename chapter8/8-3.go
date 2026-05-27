4e51654a18603d99c42a267af785300dc4066db5
Artem Leshchukov <artemleshchukov02@gmail.com>
2026-05-28 02:13:58 +0300

    Try out channels

---
+++ chapter8/8-3.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"io"
+	"log"
+	"net"
+	"os"
+)
+
+func main() {
+	conn, err := net.Dial("tcp", "localhost:8000")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer conn.Close()
+
+	done := make(chan struct{})
+	go func() {
+		io.Copy(os.Stdout, conn) // NOTE: ignoring errors
+		log.Println("done")
+		done <- struct{}{}
+		// signal the main goroutine
+	}()
+	mustCopy(conn, os.Stdin)
+	tcpConn := conn.(*net.TCPConn)
+	tcpConn.CloseWrite()
+
+	<-done
+	// wait for background goroutine to finish
+}
